47615912276f8621e841678e817ccd997dd0dc44
Luna <dlramos194@gmail.com>
2020-09-02 16:32:06 -0500

    moved files

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const questions = [
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+}
 
 // function to initialize program
 function init() {
